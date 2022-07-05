cf818651c6115b3c3bc0682cd640cac03c812cfc
Gavin Niederman <gavinniederman@gmail.com>
2022-07-05 14:18:01 -0700

    Add application class. Setup project structure

---
+++ ferrogame/src/application/mod.rs
@@ -0,0 +1,17 @@
+pub struct Application {
+
+}
+
+impl Application {
+    pub fn new() -> Application {
+        Application {
+
+        }
+    }
+
+    pub fn run(&self) {
+        loop {
+            println!("Hello, world!");
+        }
+    }
+}
