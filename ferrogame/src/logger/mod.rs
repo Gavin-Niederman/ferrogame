a032bd2e8747e8bb4f96cf5ae788e0153c833d0c
Gavin Niederman <gavinniederman@gmail.com>
2022-07-15 20:50:08 -0700

    Disable writing logs to a file.

--- ferrogame/src/logger/mod.rs
+++ ferrogame/src/logger/mod.rs
@@ -24,7 +24,6 @@ pub(crate) fn setup_logger() -> Result<(), fern::InitError> {
     })
     .level(log::LevelFilter::Debug)
     .chain(std::io::stdout())
-    .chain(fern::log_file("output.log")?)
     .apply()?;
     Ok(())
 }
